6bbc9c09fbc540fb5603b1c216ffbf69fd848d19
Pierre Van Hauwaert <pierre@rtech-engineering.nl>
2022-04-17 23:44:29 +0200

    added missing file

---
+++ tools3/leonidas/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# This is the __init__.py file of leonidas,
+# and is the root of the package.
+
+"""
+leonidas module
+
+"""
+# The version as used in the setup.py and the docs conf.py
+__version__ = "0.0.1"
